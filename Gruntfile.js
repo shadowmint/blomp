d10806c39be5e076ad7fa9c392a2841a04264327
doug <linderd@iinet.net.au>
2014-02-03 22:30:42 +0800

    sass and typescript compilers

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function (grunt) {
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-ts');
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            lib: {
+                src: ['assets/css/*', '!assets/css/.placeholder', 'assets/js/*', '!assets/js/.placeholder']
+            }
+        },
+        sass: {
+            lib: {
+                options: {                       // Target options
+                    style: 'expanded'
+                },
+                files: {           
+                    'assets/css/theme.css': 'sass/theme.scss'
+                }
+            }
+        },
+        ts: {
+            lib: {
+                src: ['typescript/**/*.ts'],
+                out: 'assets/js/theme.js',
+                options: {
+                    target: 'es3',
+                    sourceMaps: false,
+                    declaration: true,
+                    removeComments: false
+                }
+            }
+        },
+        watch: {
+            ts: {
+                files: ['sass/**/*.ts'],
+                tasks: ['ts:lib']
+            },
+            sass: {
+                files: ['sass/**/*.scss'],
+                tasks: ['sass:lib']
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['clean', 'ts:lib', 'sass:lib']);
+    //grunt.registerTask('dev', ['clean', 'ts', 'exec:tests', 'connect', 'open', 'watch']);
+}
---
+++ typescript/__init__.ts
@@ -0,0 +1,2 @@
+module blomp {
+}
